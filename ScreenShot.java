1da3923fef3f17174118cc7ce3a3f96c50bc0611
balumahedran321 <54859142+balumahedran321@users.noreply.github.com>
2020-03-25 20:44:08 +0530

    ScreenShot
    GetScreenShot

---
+++ ScreenShot.java
@@ -0,0 +1,17 @@
+package SeleniumBasic;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ScreenShot {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","./drivers/chrome.exe");
+		
+
+		
+	
+	}
+
+}
